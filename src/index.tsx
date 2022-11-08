a247969bc7ccc154284d34acbf1c2549bd1c6b4c
kelvin9314 <kelvin9314@gmail.com>
2022-11-08 11:29:49 +0800

    React Suspense + SWR

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { SWRConfig } from 'swr'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from './App';
+import V1Suspense from './pages/v1-suspense-swr';
+
 import reportWebVitals from './reportWebVitals';
+
+const fetcher = (url: string) =>
+  fetch(url).then(
+    res =>
+      new Promise(resolve =>
+        setTimeout(async () => {
+          const jsonResponse = await res.json()
+
+          resolve(jsonResponse)
+        }, 2000),
+      ),
+  )
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        fetcher,
+        suspense: true,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/v1-suspense-swr" element={<V1Suspense />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </SWRConfig>
   </React.StrictMode>
 );
 
---
+++ src/pages/v1-suspense-swr.tsx
@@ -0,0 +1,15 @@
+
+
+import ProfileContainer from '../components/profile-container'
+
+const V1Suspense = () => { 
+  
+  return (
+    <div className="App">
+      <h1>Profile</h1>
+      <ProfileContainer />
+    </div>
+  )
+}
+
+export default V1Suspense
